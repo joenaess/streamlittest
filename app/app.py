99c32befb8e68914adc30a2248e2aeda94241820
Jonas Lind <jonas.lind@ai.se>
2024-05-22 12:28:49 +0000

    added st.write crap

--- app/app.py
+++ app/app.py
@@ -12,7 +12,7 @@ st.subheader("Groq på svenska", divider="rainbow", anchor=False)
 client = Groq(
     api_key=st.secrets["GROQ_API_KEY"],
 )
-
+st.write("added groq key...")
 #GROQ_API_KEY = st.secrets["GROQ_API_KEY"]
 
 # Initialize chat history and selected model
